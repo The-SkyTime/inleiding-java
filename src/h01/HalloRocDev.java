8a4b474986bed685e0fe24f7b4cb0cd2bee5c501
Tim Wijnands <tim.wijn@hotmail.nl>
2018-08-29 11:37:11 +0200

    hoi

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,6 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("Hallo klas 7A!");
+        System.out.println("Op naar het melkweg en hier ver vandaan!");
     }
 }
