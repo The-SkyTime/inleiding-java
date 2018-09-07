de2d160d2a7c5bec65f43e59ecb65da2dc3db080
Tim Wijnands <tim.wijn@hotmail.nl>
2018-09-07 09:26:44 +0200

    Praktijk Update

--- src/h06/praktijk.java
+++ src/h06/praktijk.java
@@ -7,17 +7,21 @@ public class praktijk extends Applet {
 
     double cijf1,cijf2,cijf3;
     double cijftot;
+    double cijfx;
+    double cijfz;
 
     public void init() {
         cijf1 = 5.9;
         cijf2 = 6.3;
         cijf3 = 6.9;
         cijftot = (cijf1 + cijf2 + cijf3) / 3 ;
+        cijfx = (double) cijftot * (int)10;
+        cijfz =  (int) cijfx / (double) 10;
     }
 
 
     public void paint(Graphics g) {
         // Praktijk
-        g.drawString("" + cijftot,10,10);
+        g.drawString("Het gemiddelde cijfer is = " + cijfz,10,10);
     }
 }
